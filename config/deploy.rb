65dbfdec027c6d19fae9c1839b105c36de8901f0
esmerino <jose.esmerino@gmail.com>
2013-09-01 22:56:56 -0300

    adding -d for daemon mode rails s

--- config/deploy.rb
+++ config/deploy.rb
@@ -18,7 +18,7 @@ ssh_options[:forward_agent] = true
 namespace :deploy do
 	desc "Start application"
 	task :start, :roles => :app do
-		run "cd #{current_path}; bundle exec rails s -p 8080 -e production"
+		run "cd #{current_path}; bundle exec rails s -p 8080 -e production -d"
 	end
 
 	desc "Stop application"
