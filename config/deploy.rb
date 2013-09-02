489f524b096509c12734fdbcf084016781345efb
esmerino <jose.esmerino@gmail.com>
2013-09-01 22:34:53 -0300

    Remove Sqlite

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,12 @@
 require "bundler/capistrano"
 
-server "192.241.246.187", :web, :app, :db, primary: true
+server "192.241.246.187", :web, :app, primary: true
 
-set :application, "ludmillapsicologia.com.br"
-# Next feature => remove root acces
-set :user, "root"
-set :deploy_to, "/#{user}/apps/#{application}"
+set :application, "ludmillapsicologia"
+set :user, "deployer"
+set :deploy_to, "/home/#{user}/apps/#{application}"
 set :deploy_via, :remote_cache
 set :use_sudo, false
-set :shell, '/usr/bin/bash'
 
 set :scm, "git"
 set :repository, "git@github.com:esmerino/#{application}.git"
@@ -31,14 +29,18 @@ namespace :deploy do
 	desc "Install Ruby"
 	task :ruby_install, :roles => :app do
 		run "apt-get -y update"
+		run "gem install bundler"
+	end
+
+	desc "Install Ruby"
+	task :install, :roles => :app do
+		run "apt-get -y update"
 		run "apt-get -y install build-essential zlib1g-dev libssl-dev libreadline6-dev libyaml-dev git-core"
-		run "cd /tmp"
-		run "cd /tmp;wget http://ftp.ruby-lang.org/pub/ruby/2.0/ruby-2.0.0-p0.tar.gz"
-		run "cd /tmp;tar -xvzf ruby-2.0.0-p0.tar.gz"
-		run "cd /tmp;cd ruby-2.0.0-p0"
-		run "cd /tmp;\.\/configure --prefix=/usr/local"
-		run "cd /tmp;make"
-		run "cd /tmp;make install"
+	end
+
+	desc "Adding User"
+	task :add_user, :roles => :app do
+		run "adduser deployer --ingroup admin"
 	end
 	
 end
