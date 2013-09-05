266c6f0fec3db19c4c6077d38536c5343c4b9637
esmerino <jose.esmerino@gmail.com>
2013-09-04 23:47:05 -0300

    Deploy application

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,12 @@
 require "bundler/capistrano"
 
+load "config/recipes/base"
+load "config/recipes/nginx"
+load "config/recipes/unicorn"
+load "config/recipes/nodejs"
+load "config/recipes/rbenv"
+load "config/recipes/check"
+
 server "192.241.246.187", :web, :app, primary: true
 
 set :application, "ludmillapsicologia"
@@ -14,33 +21,3 @@ set :branch, "master"
 
 default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
-
-namespace :deploy do
-	desc "Start application"
-	task :start, :roles => :app do
-		run "cd #{current_path}; bundle exec rails s -p 80 -e production -d"
-	end
-
-	desc "Stop application"
-	task :stop, :roles => :app do
-		run "ps aux|grep rails|grep -v grep|awk '{print $2}'|xargs kill -9"
-	end
-
-	desc "Install Ruby"
-	task :ruby_install, :roles => :app do
-		run "apt-get -y update"
-		run "gem install bundler"
-	end
-
-	desc "Install Essential"
-	task :install, :roles => :app do
-		run "apt-get -y update"
-		run "apt-get -y install build-essential zlib1g-dev libssl-dev libreadline6-dev libyaml-dev git-core sqlite3 libsqlite3-dev python-software-properties"
-	end
-
-	desc "Adding User"
-	task :add_user, :roles => :app do
-		run "adduser deployer --ingroup admin"
-	end
-	
-end
