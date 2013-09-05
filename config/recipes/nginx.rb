7dd21ba43da4d184a3c7b51cb35d03630e17d351
esmerino <jose.esmerino@gmail.com>
2013-09-05 01:01:26 -0300

    Deploy

--- config/recipes/nginx.rb
+++ config/recipes/nginx.rb
@@ -1,7 +1,13 @@
 namespace :nginx do
 	desc "Install latest stable realease of nginx"
 	task :install, roles: :web do
-		run "#{sudo} add-apt-repository ppa:nginx/stable"
+		run "#{sudo} add-apt-repository ppa:nginx/stable",:pty => true do |ch,stream,data|
+			if data =~/Press.\[ENTER\].to.continue/
+				ch.send_data(Capistrano::CLI.password_prompt("Press enter to continue:")+"\n")
+			else
+				Capistrano::Configuration.default_io_proc.call(ch,stream,data)
+			end
+		end
 		run "#{sudo} apt-get -y update"
 		run "#{sudo} apt-get -y install nginx"
 	end
